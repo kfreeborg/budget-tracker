5a6a223337f95cb9e95bc0ed67b9d19205fcac51
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2021-02-20 18:26:23 -0700

    reset db

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,5 +1,5 @@
 let db;
-const request = indexedDB.open("budget", 3);
+const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function (event) {
   const db = event.target.result;
